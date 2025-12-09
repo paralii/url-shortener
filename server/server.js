dee1c82755327769c8afae3741e00df786d0c5d9
paralii <sabeehsadath14@gmail.com>
2025-12-10 00:11:18 +0530

    feat(core): add security, parsing, and performance middlewares (helmet, cors, rate-limit, mongoSanitize, xss, compression, morgan)

--- server/server.js
+++ server/server.js
@@ -1,9 +1,43 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+import compression from 'compression';
+import rateLimit from 'express-rate-limit';
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+
 import { PORT } from './utils/constants.js';
+
 dotenv.config();
 
 const app = express();
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  message: "Too many requests, try again later.",
+});
+
+app.use("/api", apiLimiter);
+app.use(cors({
+    origin : process.env.CLIENT_URL,
+    credentials : true
+}));
+app.use(mongoSanitize());
+app.use(xss());
+
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan("dev"));
+};
+
+if(process.env.NODE_ENV === "production"){
+    app.use(compression());
+};
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
     res.send('HELLO WORLD')
