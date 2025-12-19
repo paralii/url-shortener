7961c07b648b6c0859478614f59ea81b6f535936
paralii <sabeehsadath14@gmail.com>
2025-12-19 23:27:21 +0530

    feat(errors): add BadRequestError domain exception

---
+++ server/src/utils/errors/BadRequestError.js
@@ -0,0 +1,10 @@
+export default class BadRequestError extends Error {
+    constructor(message = "Bad Request") {
+        super(message);
+
+        this.name = "BadRequestError";
+        this.statusCode = 400;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
